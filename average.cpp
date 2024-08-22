ef341651811c93bc4b48e0d7d6787b07cea90fc1
jcolours <shrewsburyj2@winthrop.edu>
2024-08-22 13:07:29 -0400

    Added a computer function with an array

--- average.cpp
+++ average.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
-int main() 
+#define AEEAY_MAX 100
+// estimate the average of the numbers in the list. The parameter 
+// list is an array of doubles containing values we want to average,
+// and the parameter size is the number of items stored in the list.
+double computeAverage(double list[], int size) 
 {
 	double total = 0.0;
+	for (int idx=0; idx<size; idx++) 
+		total += list[idx];
+	return (total/size);
+}
+
+int main() 
+{
+	double list[ARRAY_MAX];
 	int count = 0;
 	double value;
 	char answer;
 
 	do {
 		std::cout << "Enter a number: ";
-		std::cin >> value;
-		total += value;
+		std::cin >> list[count];
 		count++;
 
 		std::cout << "Are you done? (y/n) ";
@@ -20,7 +31,7 @@ int main()
 	} while (answer != 'y');
 
 
-	std::cout << "The average was " << (total/count) << std::endl;
+	std::cout << "The average was " << computerAverage(list, count) << std::endl;
 
 	// everything is okay
 	return 0;
